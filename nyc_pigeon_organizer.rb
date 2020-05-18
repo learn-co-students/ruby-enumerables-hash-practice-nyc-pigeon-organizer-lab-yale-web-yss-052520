87889424792269aae9fcb297b40e32cbcd73d550
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 20:22:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value| 
+    value.each do |key2, array|
+      array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][key] == nil 
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key].push(key2.to_s)
+      end
+    end
+  end
+  return pigeon_list
 end
+
